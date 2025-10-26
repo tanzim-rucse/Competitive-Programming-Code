c4b93adcc7c662f7004100d9d25be3164e615bb1
tanzim-rucse <tanzimislam1212@gmail.com>
2025-10-26 19:56:14 +0600

    Hello this is the print of hello world

---
+++ Hello_world.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pi (3.141592653589)
+#define pb push_back
+#define ff first
+#define ss second
+#define ll long long int
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define endl "\n"
+int solve() {
+    cout<<"THis is tanzim , i am working on github. thanks...";
+
+
+return 0;
+}
+
+
+
+int main() {
+    fast_io;
+    solve();
+    return 0;
+}
